61b265ba3779b58fa28b920a9a6d721a5b8e4b29
WesleyMeacham <84281265+WesleyMeacham@users.noreply.github.com>
2021-10-30 14:06:13 -0500

    Updated usersRouter and users.js

--- db/users.js
+++ db/users.js
@@ -4,130 +4,104 @@ const bcrypt = require("bcrypt");
 // Create User Function
 
 async function createUser({ email, username, password }) {
-  try {
-    const SALT_COUNT = 10;
-    const hashedpassword = await bcrypt.hash(password, SALT_COUNT);
-    const {
-      rows: [user],
-    } = await client.query(
-      `
+	try {
+		const SALT_COUNT = 10;
+		const hashedpassword = await bcrypt.hash(password, SALT_COUNT);
+		const {
+			rows: [user],
+		} = await client.query(
+			`
         INSERT INTO users(email, username, password)
         VALUES($1, $2, $3)
         RETURNING *;
         `,
-      [email, username, hashedpassword]
-    );
-    console.log(user);
+			[email, username, hashedpassword]
+		);
+		console.log(user);
 
-    delete user.password;
+		delete user.password;
 
-    return user;
-  } catch (error) {
-    console.log("error creating user");
-    throw error;
-  }
+		return user;
+	} catch (error) {
+		console.log("error creating user");
+		throw error;
+	}
 }
 
 // Get user by username
 
 async function getUserByUsername(username) {
-  try {
-    const {
-      rows: [user],
-    } = await client.query(
-      `
+	try {
+		const {
+			rows: [user],
+		} = await client.query(
+			`
 		SELECT * FROM users
 		WHERE username=$1
 		`,
-      [username]
-    );
-  } catch (error) {
-    console.log("Error getting user by username");
-    throw error;
-  }
+			[username]
+		);
+	} catch (error) {
+		console.log("Error getting user by username");
+		throw error;
+	}
 }
 
 // Get user by email
 
 async function getUserByEmail(email) {
-  try {
-    const {
-      rows: [user],
-    } = await client.query(
-      `
+	try {
+		const {
+			rows: [user],
+		} = await client.query(
+			`
         SELECT * FROM users
         WHERE email=$1
         `,
-      [email]
-    );
+			[email]
+		);
 
-    return user;
-  } catch (error) {
-    console.log("error getting user by email");
-    throw error;
-  }
+		return user;
+	} catch (error) {
+		console.log("error getting user by email");
+		throw error;
+	}
 }
 
 // Get user by id.
 
 async function getUserById(id) {
-  try {
-    const {
-      rows: [user],
-    } = await client.query(
-      `
+	try {
+		const {
+			rows: [user],
+		} = await client.query(
+			`
         SELECT * FROM users
         WHERE id=$1
         `,
-      [id]
-    );
+			[id]
+		);
 
-    return user;
-  } catch (error) {
-    console.log("error getting user by id");
-    throw error;
-  }
+		return user;
+	} catch (error) {
+		console.log("error getting user by id");
+		throw error;
+	}
 }
 
-// This is a function to get all the users. We might not need this one but I guess we have it if we do.
+// This is a function to get all the users.
 async function getAllUsers() {
-  try {
-    const {
-      rows: [users],
-    } = await client.query(`
+	try {
+		const users = await client.query(`
         SELECT * FROM users;
         `);
-  } catch (error) {
-    console.log("error in getting all users");
-    throw error;
-  }
+		return users.rows;
+	} catch (error) {
+		console.log("error in getting all users");
+		throw error;
+	}
 }
 
-/*
-// Get user
-async function getUser({ username, password }) {
-  try {
-    const user = await getUserByUsername(username);
-    const hashedpassword = user.password;
-    const isMatch = await bcrypt.compare(password, hashedpassword);
-    if (isMatch) {
-      delete user.password;
-      return user;
-    }
-  } catch (error) {
-    console.log("error in get user");
-    throw error;
-  }
-}
-
-module.exports = {
-  createUser,
-  getAllUsers,
-  getUser,
-  getUserByEmail,
-  getUserById,
-  getUserByUsername,
-*/
 // Verify User.
 // This function is called by the usersRouter.post login function.
 // Email and password are passed in. getUserByEmail is called to get the hashed password from the DB.
@@ -154,11 +128,45 @@ async function verifyUser(email, password) {
 	}
 }
 
+async function getOrdersByUserId(id) {
+	try {
+		const orders = await client.query(
+			`
+    SELECT * FROM orders
+    WHERE user_id = $1;
+    `[id]
+		);
+		return orders.rows;
+	} catch (error) {
+		console.log("Error in get Orders By User Id");
+		throw error;
+	}
+}
+
+async function getSingleOrderByUserId(userId, orderId) {
+	try {
+		const order = await client.query(
+			`
+    SELECT * FROM orders
+    WHERE user_id = $1 AND id = $2;
+    `,
+			[userId, orderId]
+		);
+
+		return order.rows;
+	} catch (error) {
+		console.log("Error getting an order for a user.");
+		throw error;
+	}
+}
+
 module.exports = {
 	createUser,
 	getAllUsers,
+	getOrdersByUserId,
 	getUserByEmail,
 	getUserById,
 	getUserByUsername,
+	getSingleOrderByUserId,
 	verifyUser,
 };
