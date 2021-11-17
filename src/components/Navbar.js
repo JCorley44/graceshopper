f18ee03922ae0a82613179b7d4fe6cf565261715
Tyler Clark <tpageclark.1026@gmail.com>
2021-11-16 21:31:52 -0600

    styling css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -55,47 +55,61 @@ function Navbar(props) {
   };
   return (
     <div className="navbar">
-      <div>
+      <div id="navimg">
         <img
           src="
           https://th.bing.com/th/id/R.35981308b75b7604a88687c94f923ef3?rik=vN1ucLyXS2Y3PQ&riu=http%3a%2f%2fimages4.fanpop.com%2fimage%2fphotos%2f16300000%2ftheres-alot-of-them-its-so-cool-pikachu-vs-raichu-16366712-100-100.gif&ehk=5D6CKi62EyfLqHXRniR7D0jK8sycyita7zO8UT2SCTE%3d&risl=&pid=ImgRaw&r=0"
         ></img>
       </div>
-      <h1>Welcome to Nu Silk Road</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          onChange={(e) => setSearch(e.target.value)}
-          type={"text"}
-          placeholder={"What are ya looking for?"}
-          value={search}
-        ></input>
-        <button>Search</button>
-      </form>
-      <div className="navlink">
+      <div id="navmain">
         {" "}
-        <Link to="/">Home</Link> |{" "}
-        {!props.user && (
-          <>
-            {" "}
-            <Link to="/sign-in">Sign In</Link> |
-            <Link to="/register">Register</Link>
-          </>
-        )}
-        {props.user && (
-          <>
-            <Link onClick={handleLogout} to="/">
-              Log Out
-            </Link>
-          </>
-        )}
-        | <Link to="/my-cart">My Cart</Link> |{" "}
-        <Link to="/my-orders">My Orders</Link>
-        {props.userAdmin && (
-          <>
-            | <Link to="/admin">Admin Page</Link>
-          </>
-        )}
+        <h1>Welcome to Nu Silk Road</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            type={"text"}
+            placeholder={"What are ya looking for?"}
+            value={search}
+          ></input>
+          <button>Search</button>
+        </form>
+        <div className="navlink">
+          {" "}
+          <Link to="/">
+            <button>Home</button>
+          </Link>{" "}
+          {!props.user && (
+            <>
+              {" "}
+              <Link to="/sign-in">
+                <button>Sign In</button>
+              </Link>{" "}
+              <Link to="/register">
+                <button>Register</button>
+              </Link>
+            </>
+          )}
+          {props.user && (
+            <>
+              <Link onClick={handleLogout} to="/">
+                <button>Log Out</button>
+              </Link>
+            </>
+          )}{" "}
+          <Link to="/my-cart">
+            <button>My Cart</button>
+          </Link>{" "}
+          <Link to="/my-orders">
+            <button>My Orders</button>
+          </Link>
+          {props.userAdmin && (
+            <>
+              <Link to="/admin">Admin Page</Link>
+            </>
+          )}
+        </div>
       </div>
+      <div id="filler"></div>
     </div>
   );
 }
